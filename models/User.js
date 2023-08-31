88006c6159e36e6978d6a050816353633e98114a
Alan Lai <dev.alan.au@gmail.com>
2023-08-31 22:01:51 +1000

    Add function for returning client data

--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const crypto = require("node:crypto");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const secret = process.env.JWT_SECRET;
+
 const UserSchema = new mongoose.Schema(
   {
     username: {
@@ -32,23 +34,21 @@ const UserSchema = new mongoose.Schema(
 
 UserSchema.plugin(uniqueValidator, { message: "is already taken." });
 
-UserSchema.methods.setPassword = function (password) {
+UserSchema.methods.setPassword = function setPassword(password) {
   this.salt = crypto.randomBytes(16).toString("hex");
   this.hash = crypto
     .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
     .toString("hex");
 };
 
-UserSchema.methods.validatePassword = function (password) {
+UserSchema.methods.validatePassword = function validatePassword(password) {
   const hash = crypto
     .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
     .toString("hex");
   return this.hash === hash;
 };
 
-const secret = process.env.JWT_SECRET;
-
-UserSchema.methods.generateJWT = function () {
+UserSchema.methods.generateJWT = function generateJWT() {
   const today = new Date();
   const expiry = new Date().setDate(today.getDate() + 60);
   return jwt.sign(
@@ -59,6 +59,16 @@ UserSchema.methods.generateJWT = function () {
     },
     secret
   );
+};
+
+UserSchema.methods.toAuthJSON = function toAuthJSON() {
+  return {
+    username: this.username,
+    email: this.email,
+    token: this.generateJWT(),
+    bio: this.bio,
+    image: this.image,
+  };
 };
 
 mongoose.model("User", UserSchema);
