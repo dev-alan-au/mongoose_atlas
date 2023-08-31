9aea9936746594599d4eaff9fa6399e6149e6328
Alan Lai <dev.alan.au@gmail.com>
2023-08-31 21:54:39 +1000

    Add email utils

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
+const crypto = require("node:crypto");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -9,6 +10,7 @@ const UserSchema = new mongoose.Schema(
       required: [true, "can't be blank"],
       match: [/^[a-zA-Z0-9]+$/, "is invalid"],
       index: true, // for optimisation
+      unique: true,
     },
     email: {
       type: String,
@@ -16,6 +18,7 @@ const UserSchema = new mongoose.Schema(
       required: [true, "can't be blank"],
       match: [/\S+@\S+\.\S+/, "is invalid"],
       index: true, // for optimisation
+      unique: true,
     },
     bio: String,
     image: String,
@@ -26,5 +29,19 @@ const UserSchema = new mongoose.Schema(
 );
 
 UserSchema.plugin(uniqueValidator, { message: "is already taken." });
+
+UserSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+    .toString("hex");
+};
+
+UserSchema.methods.validatePassword = function (password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+    .toString("hex");
+  return this.hash === hash;
+};
 
 mongoose.model("User", UserSchema);
