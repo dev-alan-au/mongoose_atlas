6b10b62c2f33d2b2bd24ec68b21b3b6639e39beb
Alan Lai <dev.alan.au@gmail.com>
2023-09-06 15:09:29 +1000

    Add JWT decode

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser").json();
+const JSONWebToken = require("jsonwebtoken");
 // const https = require("https");
 // const http = require("http");
 require("dotenv").config();
 require("./models/User");
 require("./config/passport");
+
+const secret = process.env.JWT_SECRET;
 
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
@@ -21,6 +24,31 @@ async function main() {
 
 const app = express();
 // app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, _, next) {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    JSONWebToken.verify(
+      req.headers.authorization.split(" ")[1],
+      secret,
+      function (err, decode) {
+        if (err) {
+          req.user = undefined;
+        } else {
+          req.user = decode;
+        }
+        next();
+      }
+    );
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+
 app.use(bodyParser);
 app.use(express.json());
 app.use("/api/auth", authRouter);
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const JSONWebToken = require("jsonwebtoken");
 
 const { expressjwt: jwt } = require("express-jwt");
 require("dotenv").config();
@@ -8,13 +9,13 @@ const User = require("../models/User");
 
 const secret = process.env.JWT_SECRET;
 
-function getTokenFromHeader(req) {
-  if (req.headers?.authorization?.split(" ")[0] === "Token") {
-    return req.headers.authorization.split(" ")[1];
-  }
+// function getTokenFromHeader(req) {
+//   if (req.headers?.authorization?.split(" ")[0] === "Token") {
+//     return req.headers.authorization.split(" ")[1];
+//   }
 
-  return null;
-}
+//   return null;
+// }
 
 // const auth = {
 //   required: jwt({
@@ -32,21 +33,45 @@ function getTokenFromHeader(req) {
 // };
 
 router.post("/login", async (req, res) => {
-  const user = await User.findOne({ email: req.body.email }).exec();
+  const { email, password } = req.body ?? {};
+  if (!email || !password) {
+    return res.status(400).send({
+      message: "Email and password required.",
+    });
+  }
+
+  const user = await User.findOne({ email }).exec();
 
   if (user === null) {
     return res.status(400).send({
       message: "User not found.",
     });
   }
-  if (user.validatePassword(req.body.password)) {
-    return res.status(201).send({
-      message: "User Logged In",
+
+  if (user.validatePassword(password)) {
+    const { username, email } = user;
+    const token = JSONWebToken.sign({ username, email }, secret);
+
+    return res.status(201).json({
+      success: true,
+      token,
     });
   }
+
   return res.status(400).send({
-    message: "Wrong Password",
+    success: false,
+    message: "Wrong Password.",
   });
+});
+
+router.get("/profile", (req, res) => {
+  if (!req.user) {
+    return res.status(401).send({
+      message: "Not authorised.",
+    });
+  }
+
+  return res.status(200).send();
 });
 
 module.exports = router;
